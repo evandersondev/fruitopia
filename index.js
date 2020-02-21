3e824d76068551814c54cdd4ac0a456361271198
Evanderson Vasconcelos <evandersonvasconcelos23@gmail.com>
2020-02-21 01:00:49 -0300

    updating resposivity

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   return res.render("index");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log("Server is running...");
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,3 @@
 document.querySelector(".menu").addEventListener("click", () => {
-  const toggleMenu = document.querySelector(".toggle-menu");
-  toggleMenu.style.display = "flex";
-  toggleMenu.style.left = 0;
-  toggleMenu.style.opacity = 1;
+  document.querySelector(".toggle-menu").classList.toggle("hide");
 });
